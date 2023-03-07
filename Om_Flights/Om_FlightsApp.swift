3137801b00b41f59a6f1d4fd457115401ea26ec7
om <omchevli@gmail.com>
2023-03-07 16:14:03 -0500

    Initial Commit

---
+++ Om_Flights/Om_FlightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Om_FlightsApp.swift
+//  Om_Flights
+//
+//  Created by Om Chevli on 2023-03-07.
+//
+
+import SwiftUI
+
+@main
+struct Om_FlightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
